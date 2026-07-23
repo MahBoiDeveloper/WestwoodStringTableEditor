baced1847fe8abd5a22b044775aea243b14f9866
mah_boi <mah.boi.developer@gmail.com>
2026-07-23 18:16:44 +0300

    Add theme controls

--- Source/WWSTE.Avalonia/Views/MainWindow.axaml.cs
+++ Source/WWSTE.Avalonia/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Styling;
 
 namespace WWSTE.Avalonia.Views;
 
@@ -8,4 +11,13 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    private void MiLight_Click(object? sender, RoutedEventArgs e)
+        => Application.Current!.RequestedThemeVariant = ThemeVariant.Light;
+
+    private void MiDark_Click(object? sender, RoutedEventArgs e)
+        => Application.Current!.RequestedThemeVariant = ThemeVariant.Dark;
+
+    private void MiSystem_Click(object? sender, RoutedEventArgs e)
+        => Application.Current!.RequestedThemeVariant = ThemeVariant.Default;
 }
