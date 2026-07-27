39fccb6161eca1821fea27a244f7465e72b37e20
mah_boi <mah.boi.developer@gmail.com>
2026-07-27 23:48:08 +0300

    Begin setting implementation

---
+++ Source/WWSTE.Avalonia/Models/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WWSTE.Avalonia.Models;
+
+public struct Settings
+{
+    public string Theme = string.Empty;
+
+    public Settings()
+    {
+    }
+}
---
+++ Source/WWSTE.Avalonia/Models/UserSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Rampastring.Tools;
+using Rampastring.Tools.Ini;
+
+namespace WWSTE.Avalonia.Models;
+
+public class UserSettings
+{
+    private readonly IniDeserializationOptions _options = new()
+    {
+        SectionName = nameof(Settings),
+        SkipEmptyKeys = true,
+        SkipUnableToParseTypes = true,
+    };
+
+    private readonly IniSerializer srz = new(new Conversions());
+    public Settings Data;
+
+    UserSettings()
+    {
+        Data = srz.Deserialize<Settings>(new IniFile($"Resources/{nameof(Settings)}.ini"));
+    }
+}
